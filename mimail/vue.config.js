e30727a311739b6c81b5c30c4399540b897c3e5c
lilei <1213564228@qq.com>
2020-02-24 15:41:16 +0800

    项目目录结构划分

---
+++ mimail/vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    devServer:{
+        host:'localhost',
+        port:8080,
+        open:true,
+        proxy:{
+            //设置代理
+            '/activity':{
+                target:'https://www.wanandroid.com',
+                // changeOrigin:是否要将主机头的原点更改为目标头url地址
+                changeOrigin:true,
+                pathRewrite:{
+                    '/activity':'/activity'
+                }
+            }
+        }
+    }
+}
