d08f18e3b5057436f965ba936ec7dd44b2068f2a
lilei <1213564228@qq.com>
2020-03-09 23:11:14 +0800

    ads广告位和banner区

--- mimail/src/pages/index.vue
+++ mimail/src/pages/index.vue
@@ -3,6 +3,43 @@
     <div class="container">
       <!-- 轮播图 -->
       <div class="swiper-box">
+        <!-- 左侧列表 -->
+        <div class="classify-list">
+          <ul>
+            <li class="classify-item">
+              <a href="javascript:;">手机 电话卡</a>
+              <div class="children-list"></div>
+            </li>
+            <li class="classify-item">
+              <a href="javascript:;">电视 盒子</a>
+              <div class="children-list"></div>
+            </li>
+            <li class="classify-item">
+              <a href="javascript:;">笔记本 平板</a>
+              <div class="children-list"></div>
+            </li>
+            <li class="classify-item">
+              <a href="javascript:;">家电 插线板</a>
+              <div class="children-list"></div>
+            </li>
+            <li class="classify-item">
+              <a href="javascript:;">出行 穿戴</a>
+              <div class="children-list"></div>
+            </li>
+            <li class="classify-item">
+              <a href="javascript:;">智能 路由器</a>
+              <div class="children-list"></div>
+            </li>
+            <li class="classify-item">
+              <a href="javascript:;">电源 配件</a>
+              <div class="children-list"></div>
+            </li>
+            <li class="classify-item">
+              <a href="javascript:;">生活 箱包</a>
+              <div class="children-list"></div>
+            </li>
+          </ul>
+        </div>
         <swiper :options="swiperOption">
           <swiper-slide v-for="(item,index) in swiperList" :key="index">
             <a :href="'/#/product/'+item.id">
@@ -13,6 +50,16 @@
           <div class="swiper-button-prev" slot="button-prev"></div>
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
+      </div>
+      <!-- 下面4个广告位 -->
+      <div class="ads-box">
+        <div class="ads-item" v-for="(item,index) in adsList" :key="index">
+          <a :href="'/#/product/'+item.id"><img :src="item.url"></a>
+        </div>
+      </div>
+      <!-- 广告位下的banner图 -->
+      <div class="banner-box">
+        <a :href="'/#/product/'+bannerData.id"><img :src="bannerData.url" alt=""></a>
       </div>
     </div>
     <service-bar></service-bar>
@@ -70,7 +117,31 @@ export default {
           id: "47",
           imgUrl: "/imgs/slider/slide-4.jpg"
         }
-      ]
+      ],
+      // 广告位列表数据
+      adsList:[
+        {
+          id:"33",
+          url:"/imgs/ads/ads-1.png"
+        },
+        {
+          id:"48",
+          url:"/imgs/ads/ads-2.jpg"
+        },
+        {
+          id:"45",
+          url:"/imgs/ads/ads-3.png"
+        },
+        {
+          id:"47",
+          url:"/imgs/ads/ads-4.jpg"
+        }
+      ],
+      // banner位
+      bannerData:{
+        id:"30",
+        url:'/imgs/banner-1.png'
+      }
     };
   }
 };
@@ -78,9 +149,13 @@ export default {
 
 <style lang="scss" scoped>
 @import ".././assets/scss/base.scss";
+@import ".././assets/scss/config.scss";
+@import ".././assets/scss/mixin.scss";
 .index {
   .container {
+    // 轮播图
     .swiper-box {
+      position: relative;
       .swiper-container {
         height: 451px;
         .swiper-icon {
@@ -89,6 +164,67 @@ export default {
         }
         .swiper-button-prev{
           left: 275px;
+        }
+      }
+      .classify-list{
+        width: 264px;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 1000;
+        background: rgba(0,0,0,.3);
+        padding: 26px 0;
+        box-sizing: border-box;
+        .classify-item{
+          a{
+            display: inline-block;
+            height: 50px;
+            width: 100%;
+            font-size: 16px;
+            line-height: 50px;
+            color:$colorG ;
+            padding-left: 30px;
+            box-sizing: border-box;
+            position: relative;
+            &::after{
+              display: inline-block;
+              content: ' ';
+              @include bgImg(10px,15px,'/imgs/icon-arrow.png'); // 替换成我们定义的mixin中的bgImg()函数
+              position: absolute;
+              right: 30px;
+              // 当定位的元素需要设置居中时最好的方法：1、距离父容器50%  2、再移动-50%的自身元素宽度：transform:translateX|Y(-50%):
+              top: 50%;
+              transform: translateY(-50%);
+            }
+          }
+        }
+        .classify-item:hover{
+          background: $colorA;
+        }
+      }
+    }
+    //4个广告位
+    .ads-box{
+      @include flex();  // 使用mixin中封装的flex()函数
+      // display: flex;
+      // justify-content: space-between;
+      margin: 14px 0 31px 0;
+      a{
+        display: inline-block;
+        img{
+          width: 296px;
+          height: 167px;
+        }
+      }
+    }
+    //banner
+    .banner-box{
+      margin-bottom: 50px;
+      a{
+        img{
+          width: 100%;
+          height: 130px;
         }
       }
     }
